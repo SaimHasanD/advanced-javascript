b6dd328196a5a7c60d36ee3ffb13801e27556d61
SaimHasanD <saimhasan514@gmail.com>
2022-03-04 23:07:43 +0600

    finish

--- break-continue.js
+++ break-continue.js
@@ -6,11 +6,11 @@ for(let i = 0; i < numbers.length; i++){
     }
     console.log("break", numbers[i]);
 }
-// if condition is true continue will ignore that element  going forward the loop.
+
 const nums = [1,-2,3,4,-5,-6,7,-8,9];
 for(let i = 0; i < nums.length; i++){
     if(nums[i]<0){
-        continue;
+        continue; // if condition is true continue will ignore that element  going forward the loop.
     }
     console.log("continue", nums[i]);
 }
