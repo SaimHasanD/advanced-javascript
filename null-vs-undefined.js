dec2b0cd1c61a8ee2879c7307b6c366a232efc63
SaimHasanD <saimhasan514@gmail.com>
2022-03-04 15:50:55 +0600

    Null vs Undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,32 @@
+// if var value not declared
+let pakhi;
+console.log(pakhi);
+// if not using return in the function  
+function add(num1, num2){
+    console.log(num1 + num2);
+    return 
+}
+const result = add(10,12);
+console.log(result);
+
+// if parameter not declared 
+function adding(num1, num2){
+    console.log(num1, num2);
+}
+
+const results = adding(10);
+console.log(results);
+
+// if object value not found
+const premik = {name:"smart dude", phone:458421};
+console.log(premik.gf);
+
+// if var value = undefined, usually not recommended to do this
+let fun = undefined;
+console.log(fun);
+
+// if console.log in object of a array which on is not defined 
+let ages = [2, 5, 9]
+console.log(ages[11]);
+
+// null = not existent, usually u have to set null,if need null in output
